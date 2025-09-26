48c689ab5f81f70b1e4180d62a74df138edb9581
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-09-26 23:56:11 +0100

    bootstrap added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 import App from './App.vue'
 import router from './router'
