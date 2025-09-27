81aeba91ccee328fc79d42e631046490e658e35c
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-09-27 23:40:28 +0100

    SearchBar component created

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineEmits, ref, type Ref } from 'vue'
+
+const searchQuery: Ref<string> = ref("")
+const emit = defineEmits(["update:modelValue"])
+</script>
+
+<template>
+  <div class="row mb-3">
+    <div class="col-6">
+      <input
+        v-model="searchQuery"
+        @input="emit('update:modelValue', searchQuery)"
+        type="text"
+        class="form-control"
+        placeholder="Search by airport code, name, city or country..."
+      />
+    </div>
+
+  </div>
+</template>
