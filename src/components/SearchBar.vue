c18d2b32ca287cc0a6c9ac003cf998acaa131d25
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-02 18:32:49 +0100

    SearchBar component updated

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import { defineEmits, ref, type Ref } from 'vue'
-
-const searchQuery: Ref<string> = ref("")
-const emit = defineEmits(["update:modelValue"])
+const emit = defineEmits(["update:searchQuery"])
 </script>
 
 <template>
   <div class="row mb-3">
     <div class="col-6">
       <input
-        v-model="searchQuery"
-        @input="emit('update:modelValue', searchQuery)"
+        @input="emit('update:searchQuery', ($event.target as HTMLSelectElement).value)"
         type="text"
         class="form-control"
         placeholder="Search by airport code, name, city or country..."
