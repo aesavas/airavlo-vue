8806b1d596ecc1784996736de80834e595092696
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-03 00:03:32 +0100

    DataTable component updated to show message while table empty

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { IAirport } from '@/types'
 
-const props = defineProps({
+defineProps({
   airports: {
     type: Array as () => IAirport[],
     required: true
@@ -13,7 +13,7 @@ const tableHeaders: string[] = [" ", "Code", "Airport Name", "City", "Country", 
 </script>
 
 <template>
-  <div class="table-wrapper hide-scrollbar">
+  <div class="table-wrapper hide-scrollbar my-3">
     <table class="table table-striped table-responsive table-bordered">
       <thead>
         <tr>
@@ -25,16 +25,26 @@ const tableHeaders: string[] = [" ", "Code", "Airport Name", "City", "Country", 
         </tr>
       </thead>
       <tbody class="table-group-divider">
-        <tr
-          v-for="(airport, index) in airports"
-          :key="airport.code"
-        >
-          <td>{{ index + 1 }}</td>
-          <td>{{ airport.code }}</td>
-          <td>{{ airport.name }}</td>
-          <td>{{ airport.city }}</td>
-          <td>{{ airport.country }}</td>
-          <td><button class="btn btn-link link-offset-2 link-underline link-underline-opacity-0">🔍 More info</button>
+        <template v-if="airports && airports.length > 0">
+          <tr
+            v-for="(airport, index) in airports"
+            :key="airport.code"
+          >
+            <td>{{ index + 1 }}</td>
+            <td>{{ airport.code }}</td>
+            <td>{{ airport.name }}</td>
+            <td>{{ airport.city }}</td>
+            <td>{{ airport.country }}</td>
+            <td><button class="btn btn-link link-offset-2 link-underline link-underline-opacity-0">🔍 More info</button>
+            </td>
+          </tr>
+        </template>
+        <tr v-else>
+          <td
+            :colspan="tableHeaders.length"
+            class="text-center p-4"
+          >
+            There is no data to display.
           </td>
         </tr>
       </tbody>
