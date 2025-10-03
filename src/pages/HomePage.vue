4ef4f4f29232cad7199244d8e0e584a5d3b8860f
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-03 13:30:56 +0100

    HomePage updated

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import DataFilters from '@/components/DataFilters.vue'
+import DataTable from '@/components/DataTable.vue'
+import NavBar from '@/components/NavBar.vue'
+import SearchBar from '@/components/SearchBar.vue'
+import SortingDropdown from '@/components/SortingDropdown.vue'
+import { useAirportStore } from '@/stores/airports'
+import { storeToRefs } from 'pinia'
 
+const airportStore = useAirportStore()
+
+const { filteredAndSortedAirports } = storeToRefs(airportStore)
 </script>
 
 <template>
-  <h1>Home Page</h1>
+  <NavBar />
+  <main class="container mt-4">
+    <SearchBar />
+    <DataFilters />
+    <SortingDropdown />
+    <DataTable :airports="filteredAndSortedAirports" />
+  </main>
 </template>
-
-<style scoped></style>
