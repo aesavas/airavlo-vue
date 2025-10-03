7695ffef05c2a4d29e3edb3fd2073e02aca3bdd0
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-03 13:29:44 +0100

    AirlineList and AirlineModal components created

---
+++ src/components/AirlineList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps({
+  majorAirlines: Array<string>
+})
+</script>
+
+<template>
+  <div
+    class="accordion mt-2 mb-3"
+    id="accordion"
+  >
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#flush-collapseOne"
+          aria-expanded="false"
+          aria-controls="flush-collapseOne"
+        >
+          <p><strong>Major Airlines:</strong></p>
+        </button>
+      </h2>
+      <div
+        id="flush-collapseOne"
+        class="accordion-collapse collapse"
+        data-bs-parent="#accordion"
+      >
+        <div class="accordion-body">
+          <div
+            v-for="value in majorAirlines"
+            :key="value"
+          >🛫 {{ value }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/AirportModal.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import type { IAirport } from '@/types'
+import AirlineList from './AirlineList.vue'
+
+// 1. Define the props interface
+interface Props {
+  airport: IAirport | null
+  show?: boolean
+}
+
+// 2. Use withDefaults to provide default values
+withDefaults(defineProps<Props>(), {
+  show: false,
+  airport: null
+})
+
+// defineProps<{
+//   airport: {
+//     type: IAirport | null
+//     default: null
+//   }
+//   show?: {
+//     type: boolean
+//     default: false
+//   }
+// }>()
+
+defineEmits(['close'])
+</script>
+
+<template>
+  <!-- Main modal container -->
+  <div
+    class="modal fade"
+    :class="{ 'show': show }"
+    :style="{ 'display': show ? 'block' : 'none' }"
+    tabindex="-1"
+  >
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div
+        v-if="airport"
+        class="modal-content"
+      >
+        <div class="modal-header">
+          <h5 class="modal-title">{{ airport.name }} ({{ airport.code }})</h5>
+          <!-- 4. Emit the 'close' event on click -->
+          <button
+            type="button"
+            class="btn-close"
+            @click="$emit('close')"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-6">
+              <p><strong>City:</strong> {{ airport.city }}</p>
+              <p><strong>Country:</strong> {{ airport.country }}</p>
+              <p><strong>Continent:</strong> {{ airport.continent }}</p>
+            </div>
+            <div class="col-6">
+              <p><strong>Year Opened:</strong> {{ airport.yearOpened }}</p>
+              <p><strong>Type:</strong> {{ airport.type }}</p>
+              <p><strong>Passenger Volume:</strong> {{ airport.passengerVolume }}</p>
+            </div>
+          </div>
+          <div class="row">
+            <p><strong>Website:</strong> <a :href="airport.website">{{ airport.website }}</a></p>
+          </div>
+          <div class="row">
+            <AirlineList :major-airlines="airport.majorAirlines" />
+          </div>
+          <div class="row">
+            <iframe
+              width="300"
+              height="200"
+              frameborder="0"
+              style="border:0"
+              :src="`https://www.google.com/maps?q=${airport.coordinates.lat},${airport.coordinates.lng}&z=15&output=embed`"
+              allowfullscreen
+            >
+            </iframe>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Backdrop element required by Bootstrap -->
+  <div
+    v-if="show"
+    class="modal-backdrop fade show"
+  ></div>
+</template>
